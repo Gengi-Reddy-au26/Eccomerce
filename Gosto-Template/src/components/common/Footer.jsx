a41974d397442b0b14144123c41ffa29084f3518
GANGAREDDY N <88926606+Gengi-Reddy-au26@users.noreply.github.com>
2022-11-05 09:34:16 +0530

    project

---
+++ Gosto-Template/src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © Gangareddy  all rights reserved. Powered by gangareddy N.</p>
+      </footer>
+    </>
+  )
+}
